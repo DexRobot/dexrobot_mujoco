f2c905c62ae6c3ecfa45085682057e2d915d00d0
Yiwen Lu <mcfloundinho@gmail.com>
2024-07-30 16:09:09 +0800

    Refactor MujocoJointController to use MJControlWrapper
    This commit introduces a significant refactor of the MujocoJointController class. The changes include:
    - Replacing the direct use of mujoco and mujoco.viewer with a new MJControlWrapper class, which abstracts the control and viewer setup.
    - The import statement for load_meshes has been updated to reflect the new location under mjcf_utils.
    - A new command-line argument '--config-yaml' has been added to allow optional configuration via a YAML file.
    - The control logic within the joint callback has been simplified to use the new MJControlWrapper's send_control method.
    - The simulation stepping and viewer synchronization have been encapsulated within the MJControlWrapper's step method.
    
    Additionally, the dexrobot_urdf submodule has been updated to a new commit, which likely includes related changes to the URDF models or utilities.

--- dexrobot_mujoco.py
+++ dexrobot_mujoco.py
@@ -3,13 +3,15 @@ from rclpy.node import Node
 from sensor_msgs.msg import JointState
 import mujoco
 import mujoco.viewer
-from dexrobot_urdf.utils.mujoco_utils import load_meshes
+from dexrobot_urdf.utils.mjcf_utils import load_meshes
+from dexrobot_urdf.utils.mj_control_utils import MJControlWrapper
 import argparse
 import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('model', type=str, help='Path to the Mujoco model XML file')
 parser.add_argument('--mesh-dir', type=str, default="dexrobot_urdf/meshes/", help='Path to the directory containing the mesh files')
+parser.add_argument('--config-yaml', type=str, help='Path to the YAML file containing the configuration', required=False)
 args = parser.parse_args()
 
 
@@ -27,12 +29,11 @@ class MujocoJointController(Node):
         # Load Mujoco model
         model_path = args.model  # Replace with the path to your Mujoco model
         mesh_dir = args.mesh_dir
-        self.model = mujoco.MjModel.from_xml_path(model_path, load_meshes(mesh_dir))
-        self.data = mujoco.MjData(self.model)
-        self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
-
-        self.num_actuators = len(self.model.actuator_actnum)
-        self.actuator_names = [self.model.actuator(j).name for j in range(self.num_actuators)]
+        config_yaml = args.config_yaml
+        self.mj = MJControlWrapper(model_path, mesh_dir)
+        if config_yaml is not None:
+            self.mj.parse_yaml(config_yaml)
+        self.mj.launch_viewer("passive")
         self.get_logger().info('Mujoco Joint Controller Node has been started.')
         self.start_time = time.time()
 
@@ -41,17 +42,11 @@ class MujocoJointController(Node):
 
         for i, name in enumerate(msg.name):
             act_name = f'act_{name}'
-            if act_name in self.actuator_names:
-                actuator_index = self.actuator_names.index(act_name)
-                self.data.ctrl[actuator_index] = msg.position[i]
+            self.mj.send_control(act_name, msg.position[i])
 
         # Step the simulation to apply the control
         current_time = time.time() - self.start_time
-        while self.data.time < current_time:
-            mujoco.mj_step(self.model, self.data)
-
-        # Update the viewer
-        self.viewer.sync()
+        self.mj.step(current_time)
 
 def main(args=None):
     rclpy.init(args=args)
